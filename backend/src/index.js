32694f7b915049cc9ee6486535558cbe959b69d4
SAKSHAM <saksham832005@gmail.com>
2025-08-23 03:31:12 +0530

    Database Setup & Signup Logic

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,15 @@ import authRoutes from "./routes/auth.route.js";
 import songRoutes from "./routes/song.route.js";
 import albumRoutes from "./routes/album.route.js";
 import statsRoutes from "./routes/stat.route.js";
+import { connectDB } from "./lib/db.js";
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());//to parse incoming JSON requests like req.body
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
@@ -21,5 +24,6 @@ app.use("/api/albums", albumRoutes);
 app.use("/api/stats", statsRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ` + PORT);
+    console.log(`Server is running on port ` + PORT);
+    connectDB();
 });
