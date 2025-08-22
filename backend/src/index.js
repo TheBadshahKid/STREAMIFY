6257f7bbb93f8cc078af02cba18ed4c99bf1f286
SAKSHAM <saksham832005@gmail.com>
2025-08-23 02:10:29 +0530

    Backend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import userRoutes from "./routes/user.routes.js";
+import userRoutes from "./routes/user.route.js";
+import adminRoutes from "./routes/admin.route.js";
+
 
 
 dotenv.config();
@@ -9,6 +11,11 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use("/api/users", userRoutes);
+app.use("/api/admin", adminRoutes);
+// app.use("/api/auth", authRoutes);
+// app.use("/api/songs", songRoutes);
+// app.use("/api/albums", albumRoutes);
+// app.use("/api/stats", statsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ` + PORT);
