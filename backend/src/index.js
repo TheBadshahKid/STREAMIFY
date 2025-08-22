a902c07f32638984b46e5a921813f806e0764967
SAKSHAM <saksham832005@gmail.com>
2025-08-23 02:54:25 +0530

    MONGODB_SETUP

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
-
+import authRoutes from "./routes/auth.route.js";
+import songRoutes from "./routes/song.route.js";
+import albumRoutes from "./routes/album.route.js";
+import statsRoutes from "./routes/stat.route.js";
 
 
 dotenv.config();
@@ -12,10 +15,10 @@ const PORT = process.env.PORT;
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/songs", songRoutes);
-// app.use("/api/albums", albumRoutes);
-// app.use("/api/stats", statsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/songs", songRoutes);
+app.use("/api/albums", albumRoutes);
+app.use("/api/stats", statsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ` + PORT);
