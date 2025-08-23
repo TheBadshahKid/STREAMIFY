83f35a373adb390a7e9e375fe8dd9537ff6b2a64
SAKSHAM <saksham832005@gmail.com>
2025-08-24 01:24:01 +0530

    Made skeleton components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,25 @@ import { Toaster } from "react-hot-toast";
 import HomePage from "@/pages/home/HomePage";
 import AuthCallbackPage from "@/pages/auth-callback/AuthCallbackPage";
 import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
+import MainLayout from "./layout/MainLayout";
+import ChatPage from "./pages/chat/ChatPage";
+
 export default function App() {
   return (
     <>
       <Routes>
-        <Route path='/' element={<HomePage />} />
         <Route
 					path='/sso-callback'
 					element={<AuthenticateWithRedirectCallback signUpForceRedirectUrl={"/auth-callback"} />}
 				/>
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
-        
+        <Route element={<MainLayout />}>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/chat' element={<ChatPage />} />
+					{/* <Route path='/chat' element={<ChatPage />} />
+					<Route path='/albums/:albumId' element={<AlbumPage />} />
+					<Route path='*' element={<NotFoundPage />} /> */}
+				</Route>
       </Routes>
       <Toaster />
     </>
