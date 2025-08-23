81e5f46b9653cd1ef377eaf8aa146eae3d7fa811
SAKSHAM <saksham832005@gmail.com>
2025-08-23 14:41:57 +0530

    Creating Auth Provider & Callback Page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import HomePage from "@/pages/home/HomePage";
+import AuthCallbackPage from "@/pages/auth-callback/AuthCallbackPage";
 export default function App() {
   return (
     <>
